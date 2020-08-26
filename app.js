f51a63d8ab8bbb59f76ec37072347f8ef6d7b58c
Gyani Sunkara <gyanisunkara1@gmail.com>
2020-08-26 15:50:21 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const path = require("path");
+const express = require("express");
+const logger = require("morgan");
+
+const app = express();
+
+/**
+ * Middleware
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+process.env.NODE_ENV === "development" ? app.use(logger("dev")) : null;
+
+/**
+ * API Routes
+ */
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+
+/**
+ * Static Assets
+ */
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("public"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  });
+}
+
+module.exports = app;
