c5432f2763c2d4b7eaee9ed6bb1864394b344f2d
Gyani Sunkara <gyanisunkara1@gmail.com>
2020-08-27 13:53:39 +0530

    SignIn, SignUp & About Setup

--- client/src/app/components/signin/signin.component.ts
+++ client/src/app/components/signin/signin.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: '',
+      password: '',
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(loginData) {
+    this.userService.authUser(loginData).subscribe((data) => {
+      if (data) {
+        this.userService.setToken(data.token);
+        this.userService.setUser(data.user);
+        this.loginForm.reset();
+        this.router.navigate(['/dashboard']);
+      }
+    });
+  }
 }
--- client/src/app/components/signup/signup.component.ts
+++ client/src/app/components/signup/signup.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.registerForm = this.formBuilder.group({
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(registerData) {
+    this.userService.createUser(registerData).subscribe((data) => {
+      if (data) {
+        this.userService.setToken(data.token);
+        this.userService.setUser(data.user);
+        this.registerForm.reset();
+        this.router.navigate(['/dashboard']);
+      }
+    });
+  }
 }
